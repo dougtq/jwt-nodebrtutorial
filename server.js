7d804ec2a32f37cad9b798f5e3c74b821582f7b0
Douglas Alves <douglas.expalves@gmail.com>
2017-08-27 17:09:44 -0300

    login e session

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-let bodyParser = require('body-parser')
-let app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const JWT_PASSWORD = `notellin'`;
 
-let users = { doug : 'douglas01' }
+let app = express();
+let users = { doug: 'douglas01' };
 
-app.post('/login', bodyParser.json(), (req, res) =>{
-  return console.log( req.body )
-})
+app.post('/login', bodyParser.json(), (req, res) => {
+  if (
+    !users[req.body.username] ||
+    users[req.body.username] !== req.body.password
+  ) {
+    return res.status(401).json({ error: 'Usuário e/ou senha inválidos' });
+  } else {
+    return res.json({
+      token: jwt.sign(
+        {
+          username: req.body.username
+        },
+        JWT_PASSWORD,
+        {
+          expiresIn: '1h'
+        }
+      )
+    });
+  }
+});
 
-app.listen(3000)
+app.get('/session', (req, res) => {
+  let auth = req.headers.authorization;
+
+  if (!auth || !auth.startsWith('Bearer')) {
+    res.status(401).json({ error: 'Sessão Inválida' });
+  }
+
+  auth = auth.split('Bearer').pop().trim();
+
+  jwt.verify(auth, JWT_PASSWORD, (err, data) => {
+    if (err)
+      return res.status(401).json({ error: ' Session Impossible to decode' });
+
+    return res.json({ username : data.username });
+  });
+
+});
+
+app.listen(3000);
